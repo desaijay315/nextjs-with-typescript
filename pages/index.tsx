0f1759453a81ee3dce84e669aadfb292a9c551ae
desaijay315 <desaijay315@gmail.com>
2021-01-06 14:49:52 +0530

    added react apollo codegenerator

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,34 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import { NextPage } from "next";
 import { withApollo } from '../lib/apollo';
+import { TaskStatus, useTasksQuery } from "../generated/graphql";
 
-interface IntialProps {
-    greeting: string
-}
+interface IntialProps { }
 
 interface Props extends IntialProps { }
 
-const Home: NextPage<Props, IntialProps> = props => {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+const Home: NextPage<Props, IntialProps> = () => {
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Next.js!</a>
-                </h1>
+    const { loading, error, data } = useTasksQuery({
+        variables: {
+            status: TaskStatus.Active
+        },
+        notifyOnNetworkStatusChange: true
+    })
 
-                <p className={styles.description}>
-                    Get started by editing{' '}
-                    <code className={styles.code}>pages/index.js</code>
-                </p>
-
-                <div className={styles.grid}>
-                    <a
-                        href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className={styles.card}
-                    >
-                        <h3>Deploy &rarr;</h3>
-                        <p>
-                            {props.greeting}
-                        </p>
-                    </a>
-                </div>
-            </main>
-        </div>
-    )
+    if (loading) {
+        return <p>Loading...</p>
+    } else if (error) {
+        return <div>
+            <div> you got query failed for some reasons</div>
+            <div> {error?.message}</div>
+        </div>;
+    }
+    const tasks = data?.tasks;
+    return tasks ? <ul>{tasks.map(task => {
+        return <li key={task.id}>
+            {task.title}
+        </li>
+    })}</ul> : <p>There are no tasks here.</p>
 }
 
 Home.getInitialProps = async () => ({
